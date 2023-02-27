a4bb8b336a45e69892b7f573c99a0de20fa9ff78
dulaz41 <dulaz41@yahoo.com>
2023-02-27 18:12:47 +0100

    Initial commit

---
+++ smart_contract/hardhat.config.ts
@@ -0,0 +1,41 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const FANTOM = process.env.FANTOM;
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    fantom: {
+      chainId: 3141,
+      url: FANTOM,
+      accounts: PRIVATE_KEY ? [`0x${PRIVATE_KEY}`] : [],
+    },
+  },
+
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+};
+
+export default config;
