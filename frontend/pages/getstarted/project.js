15a7d13d6f41081b4e23713be805bc19404cf7c3
Teslim Ibisomi <ibisomimayowa@gmail.com>
2023-03-08 20:24:10 +0100

    updates

--- frontend/pages/getstarted/project.js
+++ frontend/pages/getstarted/project.js
@@ -1,9 +1,17 @@
+import Link from "next/link";
 import React from "react";
 
 const project = () => {
   return (
-    <div className="pt-20 relative   bg-[#1e2c2e] h-[960px] pb-0 mb-0 flex ">
-      <div className="  ">
+    <div className=" bg-[#1e2c2e] h-screen w-screen">
+      <div className="flex items-center text-center mr-14 justify-end ">
+        <Link href="/getstarted" className="mr-3">
+          <button className="shadow-md bg-gray-7 00 active:bg-white active:text-gray-800 hover:bg-slate-600 appearance-none border w-80 ml-9 mr-3 mt-4 rounded py-3 px-3 font-medium text-slate-50 leading-tight  hover:shadow-outline active:shadow-lg">
+            Back
+          </button>
+        </Link>
+      </div>
+      <div className=" mt-20">
         <h1 className="text-4xl  pl-48 text-slate-100 font-medium mb-4  -mt-5">
           Project Setting
         </h1>
@@ -18,7 +26,9 @@ const project = () => {
             <h3 className="text-md text-slate-400 font-bold pt-3 mt-2">
               Available Project
             </h3>
-            <div className="w-[960px] mt-4 ">{/* {to call view project} */}</div>
+            <div className="w-[960px] mt-4 ">
+              {/* {to call view project} */}
+            </div>
           </div>
         </div>
       </div>
